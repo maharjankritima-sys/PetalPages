0fc81eeb511fb1a4845cd4cf60af36fe1a916499
maharjankritima-sys <maharjankritima@gmail.com>
2026-08-06 15:30:31 +0545

    display user journals and implement journal creation workflow

--- journal/models.py
+++ journal/models.py
@@ -3,14 +3,19 @@ from django.contrib.auth.models import User
 
 
 class JournalEntry(models.Model):
+
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="journals"
     )
+
     title = models.CharField(max_length=200)
+
     content = models.TextField()
+
     created_at = models.DateTimeField(auto_now_add=True)
+
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- journal/views.py
+++ journal/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from .models import JournalEntry
 from .forms import JournalEntryForm
-
-
 
 
 @login_required
 def dashboard(request):
-    return render(request, "journal/dashboard.html")
+    journals = JournalEntry.objects.filter(owner=request.user).order_by("-created_at")
+
+    return render(request, "journal/dashboard.html", {
+        "journals": journals
+    })
+
+
 @login_required
 def create_journal(request):
 
@@ -17,15 +22,13 @@ def create_journal(request):
         form = JournalEntryForm(request.POST)
 
         if form.is_valid():
-
             journal = form.save(commit=False)
             journal.owner = request.user
             journal.save()
 
-            return redirect("home")
+            return redirect("journal_dashboard")
 
     else:
-
         form = JournalEntryForm()
 
     return render(request, "journal/create.html", {
